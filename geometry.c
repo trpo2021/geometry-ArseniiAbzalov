dc218ff399bc9031aa7e03457996dae16f7cf569
ArseniiAbzalov <a-abzalov115@yandex.ru>
2021-02-26 00:33:19 +0700

    Error correction 1.Removing unnecessary features 2.General optimization

--- geometry.c
+++ geometry.c
@@ -4,14 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* parsing_digit1(char* cursor, double* x);
-char* parsing_digit2(char* cursor, double* y);
-char* parsing_digit3(char* cursor, double* radius);
-char* parsing_comma(char* cursor);
-bool search_brecket(char* cursor);  //поиск '('
-bool search_comma(char* cursor);    //поиск ','
-bool search_brecket2(char* cursor); //поиск ')'
-bool check_eof(char* cursor); //проверка до конца строки
+char* pars_bracket(char* cursor, char sign); //парсинг скобок
+char* pars_digit(char* cursor, double* num); //парсинг чисел
+char* pars_comma(char* cursor); //парсинг запятой
+bool check_eof(char* cursor);   //проверка до конца строки
 
 int main()
 {
@@ -23,11 +19,11 @@ int main()
 
     fgets(str, sizeof(str), stdin);
 
-    while (*cursor == ' ') {
+    while (*cursor == ' ') { //пропускаем пробелы перед словом
         cursor++;
         cursor2++;
     }
-    if (isalpha(*cursor) != 0) {
+    if (isalpha(*cursor) != 0) { //ставим второй курсор на конец слова
         while (isalpha(*cursor2) != 0) {
             cursor2++;
         }
@@ -36,102 +32,83 @@ int main()
     if (strncasecmp(cursor, "circle", cursor2 - cursor) != 0) {
         printf("^\n");
         printf("Error at column 0: expected 'circle'\n");
+        return 0;
+    }
+
+    cursor = cursor2;
+
+    if ((cursor = pars_bracket(cursor, '(')) == 0)
+    {
+        return 0;
+    }
+    if ((cursor = pars_digit(cursor, &x)) != 0)
+    {
+        data[0] = x;
     } else {
-        cursor = cursor2;
+        return 0;
+    }
+    //    printf("x = %.1f\n", data[0]);
 
-        if (search_brecket(cursor) == 0) //поиск '('
-        {
-            return 0;
+    if ((cursor = pars_digit(cursor, &y)) != 0)
+    {
+        data[1] = y;
+    } else {
+        return 0;
+    }
+
+    //    printf("y = %.1f\n", data[1]);
+
+    if ((cursor = pars_comma(cursor)) == 0)
+    {
+        return 0;
+    }
+
+    if ((cursor = pars_digit(cursor, &radius)) != 0)
+    {
+        data[2] = radius;
+    } else {
+        return 0;
+    }
+    //    printf("rad = %.1f\n", data[2]);
+
+    if ((cursor = pars_bracket(cursor, ')')) == 0)
+    {
+        return 0;
+    }
+
+    if (check_eof(cursor) == 0)
+    {
+        return 0;
+    }
+
+    return 0;
+}
+
+char* pars_bracket(char* cursor, char sign)
+{
+    while (*cursor != '\n') {
+        if (*cursor == sign) {
+            cursor++;
+            return cursor;
         }
-
-        if ((cursor = parsing_digit1(cursor, &x)) != 0) //парсинг первого числа
-        {
-            data[0] = x;
+        if (*cursor == ' ') {
+            cursor++;
         } else {
+            printf("expected '%c'\n", sign);
             return 0;
         }
-//         printf("x = %.1f\n", data[0]);
-
-        if ((cursor = parsing_digit2(cursor, &y)) != 0) //парсинг второго числа
-        {
-            data[1] = y;
-        } else {
-            return 0;
-        }
-//         printf("y = %.1f\n", data[1]);
-
-        if (search_comma(cursor) == 0) //поиск ','
-        {
-            return 0;
-        }
-
-        if ((cursor = parsing_comma(cursor)) == 0) //парсинг запятой
-        {
-            return 0;
-        }
-
-        if ((cursor = parsing_digit3(cursor, &radius)) != 0) //парсинг третьего числа
-        {
-            data[2] = radius;
-        } else {
-            return 0;
-        }
-//         printf("rad = %.1f\n", data[2]);
-
-        if (search_brecket2(cursor) == 0) //поиск ')'
-        {
-            return 0;
-        }
-        if (check_eof(cursor) == 0) //проверка конца строки
-        {
-            return 0;
-        }
+    }
+    if (*cursor == '\n') {
+        printf("expected '%c'\n", sign);
+        return 0;
     }
     return 0;
 }
 
-bool search_brecket(char* cursor)
-{
-    int flag = 0;
-
-    while (*cursor != 10) {
-        if (*cursor == '(') {
-            flag = 1;
-            break;
-        }
-        cursor++;
-    }
-    if (flag == 0) {
-        printf("expected '('\n");
-        return false;
-    }
-    return true;
-}
-
-char* parsing_digit1(char* cursor, double* x)
+char* pars_digit(char* cursor, double* num)
 {
     char* end;
     while (isdigit(*cursor) == 0) {
-        if ((*cursor == ' ') || (*cursor == '(')) {
-            cursor++;
-        } else {
-            printf("expected '<double>'\n");
-            return 0;
-        }
-    }
-
-    if (isdigit(*cursor) != 0) {
-        *x = strtod(cursor, &end);
-        cursor = end;
-    }
-    return cursor;
-}
-
-char* parsing_digit2(char* cursor, double* y)
-{
-    char* end;
-    while (isdigit(*cursor) == 0) {
-
         if (*cursor == ' ') {
             cursor++;
         } else {
@@ -141,44 +118,24 @@ char* parsing_digit2(char* cursor, double* y)
     }
 
     if (isdigit(*cursor) != 0) {
-        *y = strtod(cursor, &end);
+        *num = strtod(cursor, &end);
         cursor = end;
     }
     return cursor;
 }
 
-bool search_comma(char* cursor)
-{
-    int flag = 0;
-
-    while (*cursor != 10) {
-        if (*cursor == ',') {
-            flag = 1;
-            break;
-        }
-        cursor++;
-    }
-    if (flag == 0) {
-        printf("expected ','\n");
-        return false;
-    }
-    return true;
-}
-
-char* parsing_comma(char* cursor)
+char* pars_comma(char* cursor)
 {
     while (*cursor != ',') {
-
         if (*cursor == ' ') {
             cursor++;
         } else {
-            printf("expected '<double>'\n");
+            printf("expected ','\n");
             return 0;
         }
     }
 
-    if (*cursor == ',')
-    {
+    if (*cursor == ',') {
         cursor++;
     }
 
@@ -191,65 +148,9 @@ char* parsing_comma(char* cursor)
     return cursor;
 }
 
-char* parsing_digit3(char* cursor, double* radius)
-{
-    char* end;
-
-    while (isdigit(*cursor) == 0) {
-
-        if (*cursor == ' ') {
-            cursor++;
-        } else {
-            printf("expected '<double>'\n");
-            return 0;
-        }
-    }
-
-    if (isdigit(*cursor) != 0) {
-        *radius = strtod(cursor, &end);
-        cursor = end;
-    }
-    return cursor;
-}
-
-bool search_brecket2(char* cursor)
-{
-    int flag = 0;
-
-    while (*cursor != 10) {
-        if (*cursor == ')') {
-            flag = 1;
-            break;
-        }
-        cursor++;
-    }
-    if (flag == 0) {
-        printf("expected ')'\n");
-        return false;
-    }
-    return true;
-}
-
 bool check_eof(char* cursor)
 {
-    while (*cursor != ')') {
-
-        if (*cursor == ' ') {
-            cursor++;
-        } else {
-            printf("expected '<double>'\n");
-            return false;
-        }
-    }
-
-    if (*cursor == ')') {
-        cursor++;
-        if (*cursor == 10) {
-            return true;
-        }
-    }
-
-    while (*cursor != 10) {
+    while (*cursor != '\n') {
         if (*cursor != ' ') {
             printf("unexpected token\n");
             return false;
