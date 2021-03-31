212740c1ed4d7a226c753731a81c2448d9ef3cf7
ArseniiAbzalov <a-abzalov115@yandex.ru>
2021-03-31 15:49:41 +0700

    1.Triangles removed 2.Global change

--- src/libgeometry/functions.c
+++ src/libgeometry/functions.c
@@ -8,85 +8,98 @@
 
 #define SIZE_ARR_OBJ 4
 
-static bool check_correct_triangle(struct figure_triangle triangle)
-{
-    if ((triangle.x1 != triangle.x4) || (triangle.y1 != triangle.y4)) {
-        printf("uncorrect triangle\n");
-        return false;
-    }
-    return true;
-}
-
-static double side_length(double x1, double y1, double x2, double y2)
+double side_length(double x1, double y1, double x2, double y2)
 {
     return sqrt((pow((y1 - x1), 2)) + (pow((y2 - x2), 2)));
 }
-double calc_perimeter_circle(struct figure_circle circle)
+
+void collision(
+        figure_circle* circle,
+        int num_circle,
+        int collisions[num_circle][num_circle])
+{
+    double length_between_points, sum_of_radius;
+
+    for (int i = 0; i < num_circle; i++) {
+        for (int j = 0; j < num_circle; j++) {
+            collisions[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < num_circle; i++) {
+        for (int j = 0; j < num_circle; j++) {
+            if (i != j) {
+                sum_of_radius = fabs((circle[i].radius + circle[j].radius));
+                length_between_points = side_length(
+                        circle[j].x, circle[i].x, circle[j].y, circle[i].y);
+                if (length_between_points <= sum_of_radius) {
+                    collisions[i][j] = 1;
+                }
+            }
+        }
+    }
+}
+
+double calc_perimeter_circle(figure_circle circle)
 {
     return 2 * M_PI * circle.radius;
 }
 
-double calc_square_circle(struct figure_circle circle)
+double calc_area_circle(figure_circle circle)
 {
     return M_PI * pow(circle.radius, 2);
 }
 
-double calc_perimeter_triangle(struct figure_triangle triangle)
-{
-    double a, b, c;
-
-    a = side_length(triangle.x1, triangle.x2, triangle.y1, triangle.y2);
-    b = side_length(triangle.x2, triangle.x3, triangle.y2, triangle.y3);
-    c = side_length(triangle.x3, triangle.x1, triangle.y3, triangle.y1);
-
-    return a + b + c;
-}
-
-double calc_square_triangle(struct figure_triangle triangle)
-{
-    double a, b, c, p;
-
-    a = side_length(triangle.x1, triangle.x2, triangle.y1, triangle.y2);
-    b = side_length(triangle.x2, triangle.x3, triangle.y2, triangle.y3);
-    c = side_length(triangle.x3, triangle.x1, triangle.y3, triangle.y1);
-
-    p = (a + b + c) / 2;
-    return sqrt((p * (p - a) * (p - b) * (p - c)));
-}
-
 static char* parse_bracket(char* cursor, char sign)
 {
-    while (*cursor != '\n') {
-        if (*cursor == sign) {
+    while (*cursor != sign) {
+        if (*cursor == ' ')
             cursor++;
-            return cursor;
-        }
-        if (*cursor == ' ') {
-            cursor++;
-        } else {
-            printf("expected '%c'\n", sign);
-            return 0;
-        }
+        else
+            return NULL;
     }
-    if (*cursor == '\n') {
-        printf("expected '%c'\n", sign);
-        return 0;
+    if (*cursor == sign) {
+        cursor++;
+        return cursor;
     }
-    return 0;
+    return cursor;
+
+    /*
+      while (*cursor != '\n') {
+          if (*cursor == sign) {
+              cursor++;
+              return cursor;
+          }
+          if (*cursor == ' ') {
+              cursor++;
+          } else {
+              return NULL;
+          }
+      }
+
+      if (*cursor == '\n') {
+          return NULL;
+      }
+      return NULL;
+      */
 }
 
 static char* parse_digit(char* cursor, double* num)
 {
     char* end;
     while (isdigit(*cursor) == 0) {
-        if (*cursor == ' ') {
-            cursor++;
+        if (*cursor == '-') {
+            *num = strtod(cursor, &end);
+            cursor = end;
+            return cursor;
         } else {
-            printf("expected '<double>'\n");
-            return 0;
+            if (*cursor == ' ') {
+                cursor++;
+            } else {
+                return NULL;
+            }
         }
     }
-
     if (isdigit(*cursor) != 0) {
         *num = strtod(cursor, &end);
         cursor = end;
@@ -97,23 +110,14 @@ static char* parse_digit(char* cursor, double* num)
 static char* parse_comma(char* cursor)
 {
     while (*cursor != ',') {
-        if (*cursor == ' ') {
+        if (*cursor == ' ')
             cursor++;
-        } else {
-            printf("expected ','\n");
-            return 0;
-        }
+        else
+            return NULL;
     }
-
     if (*cursor == ',') {
         cursor++;
-    }
-
-    if (*cursor != ' ') {
-        printf("expected ' ' after ','\n");
-        return 0;
-    } else {
-        cursor++;
+        return cursor;
     }
     return cursor;
 }
@@ -122,141 +126,72 @@ static char* check_eof(char* cursor)
 {
     while (*cursor != '\n') {
         if (*cursor != ' ') {
-            printf("unexpected token\n");
-            return false;
+            return NULL;
         }
         cursor++;
     }
     return cursor;
 }
 
-char* parse_circle(char* cursor, int i, struct figure_circle* circle)
+int parse_circle(
+        char str[],
+        char* startcursor,
+        char* cursor,
+        figure_circle* circle,
+        int num_circle)
 {
-    double x1, y1, radius1;
+    startcursor = str;
+    cursor = str;
+    char Circle[] = {"circle"};
 
-    if ((cursor = parse_bracket(cursor, '(')) == 0) {
-        return 0;
+    while (*startcursor == ' ') {
+        startcursor++;
+        cursor++;
     }
-    if ((cursor = parse_digit(cursor, &x1)) != 0) {
-        circle[i].x = x1;
+    if (isalpha(*startcursor) != 0) {
+        while (isalpha(*cursor) != 0) {
+            cursor++;
+        }
+    }
+    if (strncasecmp(startcursor, "circle", strlen(Circle)) != 0)
+        return -1;
+
+    double digit;
+
+    if ((cursor = parse_bracket(cursor, '(')) == NULL) {
+        return -2;
+    }
+    if ((cursor = parse_digit(cursor, &digit)) == NULL) {
+        return -3;
     } else {
-        return 0;
+        circle[num_circle].x = digit;
     }
 
-    if ((cursor = parse_digit(cursor, &y1)) != 0) {
-        circle[i].y = y1;
+    if ((cursor = parse_digit(cursor, &digit)) == NULL) {
+        return -4;
     } else {
-        return 0;
+        circle[num_circle].y = digit;
     }
 
-    if ((cursor = parse_comma(cursor)) == 0) {
-        return 0;
+    if ((cursor = parse_comma(cursor)) == NULL) {
+        return -5;
     }
 
-    if ((cursor = parse_digit(cursor, &radius1)) != 0) {
-        circle[i].radius = radius1;
+    if ((cursor = parse_digit(cursor, &digit)) == NULL) {
+        return -6;
     } else {
-        return 0;
+        if (digit <= 0)
+            return -7;
+        else
+            circle[num_circle].radius = digit;
     }
 
-    if ((cursor = parse_bracket(cursor, ')')) == 0) {
-        return 0;
+    if ((cursor = parse_bracket(cursor, ')')) == NULL) {
+        return -8;
     }
 
-    if ((cursor = check_eof(cursor)) == 0) {
-        return 0;
+    if ((cursor = check_eof(cursor)) == NULL) {
+        return -9;
     }
-
-    return cursor;
-}
-
-char* parse_triangle(char* cursor, int j, struct figure_triangle* triangle)
-{
-    double x1, x2, x3, x4, y1, y2, y3, y4;
-
-    if ((cursor = parse_bracket(cursor, '(')) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_bracket(cursor, '(')) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &x1)) != 0) {
-        triangle[j].x1 = x1;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &y1)) != 0) {
-        triangle[j].y1 = y1;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_comma(cursor)) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &x2)) != 0) {
-        triangle[j].x2 = x2;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &y2)) != 0) {
-        triangle[j].y2 = y2;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_comma(cursor)) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &x3)) != 0) {
-        triangle[j].x3 = x3;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &y3)) != 0) {
-        triangle[j].y3 = y3;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_comma(cursor)) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &x4)) != 0) {
-        triangle[j].x4 = x4;
-    } else {
-        return 0;
-    }
-
-    if ((cursor = parse_digit(cursor, &y4)) != 0) {
-        triangle[j].y4 = y4;
-    } else {
-        return 0;
-    }
-
-    if ((check_correct_triangle(triangle[j])) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_bracket(cursor, ')')) == 0) {
-        return 0;
-    }
-
-    if ((cursor = parse_bracket(cursor, ')')) == 0) {
-        return 0;
-    }
-
-    if ((cursor = check_eof(cursor)) == 0) {
-        return 0;
-    }
-
-    return cursor;
+    return 0;
 }
--- src/libgeometry/functions.h
+++ src/libgeometry/functions.h
@@ -2,26 +2,25 @@
 #define SIZE_ARR_OBJ 4
 #define SIZE_STR 64
 
-struct figure_circle {
+typedef struct {
     double x;
     double y;
     double radius;
-};
+    double perimeter;
+    double area;
+} figure_circle;
 
-struct figure_triangle {
-    double x1;
-    double y1;
-    double x2;
-    double y2;
-    double x3;
-    double y3;
-    double x4;
-    double y4;
-};
+double side_length(double x1, double y1, double x2, double y2);
 
-double calc_square_circle(struct figure_circle circle);
-double calc_perimeter_circle(struct figure_circle circle);
-double calc_perimeter_triangle(struct figure_triangle triangle);
-double calc_square_triangle(struct figure_triangle triangle);
-char* parse_circle(char* cursor, int i, struct figure_circle* circle);
-char* parse_triangle(char* cursor, int j, struct figure_triangle* triangle);
+double calc_area_circle(figure_circle circle);
+double calc_perimeter_circle(figure_circle circle);
+void collision(
+        figure_circle* circle,
+        int num_circle,
+        int collisions[num_circle][num_circle]);
+int parse_circle(
+        char str[],
+        char* startcursor,
+        char* cursor,
+        figure_circle* circle,
+        int num_circle);
